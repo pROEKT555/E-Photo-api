d3f191b293a31920b3309611881fd2a921caa22a
anonimus007-700 <sahsafg80@gmail.com>
2025-03-07 23:08:47 +0200

    it`s finali registration

--- E_Photo_API/apps/e_user/views.py
+++ E_Photo_API/apps/e_user/views.py
@@ -2,12 +2,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.contrib.auth.models import User, Group
+
 from E_Photo_API.apps.e_user.serializers import UserRegistrationSerializer
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
-        print(serializer)
         if serializer.is_valid(raise_exception=True):
+            user = User.objects.create_user(
+                username=serializer.data['username'],
+                password=serializer.data['password'],
+                email=serializer.data['email']
+            )
+
+            group, created = Group.objects.get_or_create(name=serializer.data['groups'][0])
+
+            user.groups.add(group)
+            user.save()
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # E-Photo-api
 
+`pip install requirements.txt`
+
+`python manage.py runserver`
+
 in `http://127.0.0.1:8000/register/` only **POST**:
 ```json
 {
@@ -7,7 +11,7 @@ in `http://127.0.0.1:8000/register/` only **POST**:
     "password": "zxc",
     "email": "zxc@zxc.com",
     "groups": [
-        "role"  // можна декілька, це роль
+        "role"  // можна тільки одну, це роль
     ]
 }
 ```
